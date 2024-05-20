a3cd54e7678b0cb9492b842439c415f92aced0dc
kingkazuma7 <your.nasionasio7@gmail.com>
2024-05-20 13:42:11 +0900

    seeder

--- database/seeders/MembersTableSeeder.php
+++ database/seeders/MembersTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MembersTableSeeder extends Seeder
 {
@@ -13,6 +14,23 @@ class MembersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('members')->insert(
+            [
+                [
+                    'name' => '山田',
+                    'telephone' => '123-4567',
+                    'email' => 'yamada@example.com',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ],
+                [
+                    'name' => '鈴木',
+                    'telephone' => '234-5678',
+                    'email' => 'suzuki@example.com',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ],
+            ]
+        );
     }
 }
